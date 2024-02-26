8e33ec58142003a1dccdc47aa0a444ee7bdaf194
chrialge <chrialge@gmail.com>
2024-02-26 16:15:54 +0100

    add live_code

--- live_code/assets/js/script.js
+++ live_code/assets/js/script.js
@@ -5,24 +5,26 @@ Chiedi all'utente di indovinare il numero. Dopo ogni tentativo, fornisci un sugg
 */
 
 let number = Math.floor(Math.random() * 100) + 1; 
-console.log(number)
+console.log(number);
 
-let numberTentativ = 1;
-let trovato = true
+let numberTentativ = 0;
+let trovato = true;
 do{
 
-    const numberUser = parseInt(prompt('inserisci un numero tra 1 e 100: '))
+    const numberUser = parseInt(prompt('inserisci un numero tra 1 e 100: '));
     if (number > numberUser) {
-        alert(`il numero che hai dato e minore, e il tuo ${numberTentativ} tentativo`)
+        alert(`il numero che hai dato e minore, e il tuo ${numberTentativ} tentativo`);
         
     } else if(number < numberUser){
-        alert(`il tuo numero che hai dato e maggiore, e il tuo ${numberTentativ} tentativo `)
+        alert(`il tuo numero che hai dato e maggiore, e il tuo ${numberTentativ} tentativo `);
         
     }else{
-        alert(`il tuo numero e uguale a quello nosto, hai indovinato al tuo ${numberTentativ} tentativo `)
-        trovato = false
-        location.reload(30)
-    }
-    numberTentativ++
+        alert(`il tuo numero e uguale a quello nosto, hai indovinato al tuo ${numberTentativ} tentativo `);
+        trovato = false;
+        location.reload(30);
+    };
+    
+    numberTentativ++;
+
 } while(trovato == true);
 
