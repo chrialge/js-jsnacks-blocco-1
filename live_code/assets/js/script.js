5d58e08664e20b1b13d5da322eec42f0f809543c
chrialge <chrialge@gmail.com>
2024-02-26 16:08:41 +0100

    add live_code

--- live_code/assets/js/script.js
+++ live_code/assets/js/script.js
@@ -3,6 +3,7 @@ Gioco di indovinello:
 Genera un numero casuale compreso tra 1 e 100. 
 Chiedi all'utente di indovinare il numero. Dopo ogni tentativo, fornisci un suggerimento se il numero inserito è maggiore o minore del numero segreto. Fornisci anche il numero totale di tentativi effettuati.
 */
+
 let number = Math.floor(Math.random() * 100) + 1; 
 console.log(number)
 
@@ -13,14 +14,15 @@ do{
     const numberUser = parseInt(prompt('inserisci un numero tra 1 e 100: '))
     if (number > numberUser) {
         alert(`il numero che hai dato e minore, e il tuo ${numberTentativ} tentativo`)
-        numberTentativ++
+        
     } else if(number < numberUser){
         alert(`il tuo numero che hai dato e maggiore, e il tuo ${numberTentativ} tentativo `)
-        numberTentativ++
+        
     }else{
         alert(`il tuo numero e uguale a quello nosto, hai indovinato al tuo ${numberTentativ} tentativo `)
         trovato = false
         location.reload(30)
     }
+    numberTentativ++
 } while(trovato == true);
 
