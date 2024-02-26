b62e7a267a5c7747655d2cae3cf70638f46d6fc3
chrialge <chrialge@gmail.com>
2024-02-26 13:07:06 +0100

    add snack7

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,15 +1,11 @@
 console.log('ciuao')
-const containerCube = document.querySelector('.container')
-
+let valueCube = parseInt(prompt('inserisci un numero:'))
+let list = []
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente
-for (let i = 0; i < 3; i++) {
-    let valueCube = parseInt(prompt('inserisci un numero:'))
-    const markup = `
-    <div class="box" width="100px" height="100px" background-color="green">
-        <span>${valueCube}</span>
-    </div>
-    `
-    containerCube.insertAdjacentHTML('beforeend', markup)
+for (let i = 0; i < valueCube; i++) {
+
+    
+    let cubeNumber = Math.pow(i, 3);
     
 }
 
---
+++ jsnack7/script.js
@@ -0,0 +1,16 @@
+//Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero
+
+console.log('ciao') 
+let sNumber = prompt('dammi un numero di 4 cifre: ');
+console.log(typeof sNumber);
+let sum = 0;
+let array =[]
+const lenght = sNumber.length;
+for (let i =0; i < lenght; i++){
+    const element = sNumber[i]
+    array.push(element)
+    sum += Number(element)
+}
+console.log(array, sum);
+
+
